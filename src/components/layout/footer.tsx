df9d4d6c98404a1c1b42b1c681d7cf2fb487caef
8109 <a8109@iMac-AM-09.local>
2023-06-08 15:07:48 +0900

    footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,16 +2,17 @@ import { FC } from 'react'
 import styled from 'styled-components'
 
 const Footer: FC = () => {
-  return <Wrap>footer</Wrap>
+  return <Wrap>&copy; 2020 Profile</Wrap>
 }
 
 const Wrap = styled.footer`
   margin: 10px auto;
   text-align: center;
-  font: normal normal normal 10px/15px Meiryo;
+  font-size: 10px;
+  line-height: 15px;
+  font-family:'メイリオ', 'Meiryo','MS ゴシック','Hiragino Kaku Gothic ProN','ヒラギノ角ゴ ProN W3',sans-serif;
   letter-spacing: 0;
   color: #383E45;
-  opacity: 1;
 `
 
 export default Footer
