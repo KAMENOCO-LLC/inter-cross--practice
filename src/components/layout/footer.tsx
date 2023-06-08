9b5009080c447aef2453e1c748924327fdc330ca
8109 <a8109@iMac-AM-09.local>
2023-06-08 13:42:07 +0900

    footer作成

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,6 +5,13 @@ const Footer: FC = () => {
   return <Wrap>footer</Wrap>
 }
 
-const Wrap = styled.footer``
+const Wrap = styled.footer`
+  margin: 10px auto;
+  text-align: center;
+  font: normal normal normal 10px/15px Meiryo;
+  letter-spacing: 0;
+  color: #383E45;
+  opacity: 1;
+`
 
 export default Footer
