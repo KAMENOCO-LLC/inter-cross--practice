d1944120777b594b26273d7125f34fcd3be7ddcb
8109 <a8109@iMac-AM-09.local>
2023-06-12 13:46:37 +0900

    about修正

--- src/components/pages/top/about.tsx
+++ src/components/pages/top/about.tsx
@@ -5,10 +5,11 @@ import Image from 'next/image'
 const About: FC = () => {
     return (
         <Wrap>
-            <Title>
-                About
-            </Title>
-            <Line></Line>
+            <TitleBox>
+                <Title>
+                    About
+                </Title>
+            </TitleBox>
             <FlexBox>
                 <Image src={'/images/about.jpg'} alt={'プロフィール画像'} width={100} height={100}/>
                 <Container>
@@ -26,7 +27,23 @@ const About: FC = () => {
 
 
 const Wrap = styled.section`
-  margin-top: 80px;
+  max-width: 960px;
+  padding: 0 4%;
+  margin: 80px auto 0 auto;
+`
+const TitleBox = styled.div`
+  text-align: center;
+`
+const Title = styled.h1`
+  display: inline-block;
+  color: #383E45;
+  font-size: 32px;
+  line-height: 48px;
+  font-family: 'メイリオ', 'Meiryo', 'MS ゴシック', 'Hiragino Kaku Gothic ProN', 'ヒラギノ角ゴ ProN W3', sans-serif;;
+  letter-spacing: 0;
+  border-bottom-style: solid;
+  border-bottom-color: #383E45;
+  border-bottom-width: 1px;
 `
 const FlexBox = styled.div`
   margin-top: 60px;
@@ -38,7 +55,7 @@ const FlexBox = styled.div`
     vertical-align: middle;
   }
 
-  @media (min-width: 768px ) {
+  @media (min-width: 600px ) {
     display: flex;
     justify-content: center;
     margin-top: 70px;
@@ -50,20 +67,9 @@ const FlexBox = styled.div`
   }
 `
 const Container = styled.div`
-  width: 345px;
-  margin: 0 auto;
-  @media (min-width: 768px) {
-    margin: unset;
+  @media (min-width: 600px) {
     width: 403px;
   }
-`
-const Title = styled.h1`
-  text-align: center;
-  color: #383E45;
-  font-size: 32px;
-  line-height: 48px;
-  font-family: 'メイリオ', 'Meiryo', 'MS ゴシック', 'Hiragino Kaku Gothic ProN', 'ヒラギノ角ゴ ProN W3', sans-serif;;
-  letter-spacing: 0;
 `
 const Line = styled.div`
   width: 101px;
@@ -81,7 +87,7 @@ const Name = styled.h2`
   font-family: 'メイリオ', 'Meiryo', 'MS ゴシック', 'Hiragino Kaku Gothic ProN', 'ヒラギノ角ゴ ProN W3', sans-serif;
   letter-spacing: 0;
   text-align: left;
-  @media (min-width: 768px) {
+  @media (min-width: 600px) {
     margin-top: unset;
   }
 `
@@ -96,7 +102,7 @@ const Text = styled.h3`
 `
 
 const PcBr = styled.br`
-  @media (min-width: 768px) {
+  @media (min-width: 600px) {
     display: none;
   }
 `
