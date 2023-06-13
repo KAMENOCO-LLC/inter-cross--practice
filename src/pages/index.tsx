c8a974ef4bff72885eceb7a77bed65c2eca7d1b2
8109 <a8109@iMac-AM-09.local>
2023-06-13 13:39:04 +0900

    blog修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import DefaultLayout from '@/components/layout/default-layout'
 import Bicycle from '@/components/pages/top/bicycle'
 import Mv from '@/components/pages/top/mv'
 import About from '@/components/pages/top/about'
-import Blog from "@/components/pages/top/blog";
+import Blog from '@/components/pages/top/blog'
 
 const Home: NextPageWithLayout = () => {
   return (
